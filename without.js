ab9fe639d43857bf1c1024a34a5d7ff0ce6302b2
Christopher Toy <christopherstoy@gmail.com>
2020-08-19 04:27:26 +0000

    added without function

---
+++ without.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i] || typeof arrayOne[i] !== typeof arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅ Assertion Pased: ${arrayOne} === ${arrayTwo} ✅`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo} ❌`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
