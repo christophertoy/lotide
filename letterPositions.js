9af7ffa9a0e10a66fb5be88eef9e7f4ac963de0b
Christopher <christopherstoy@gmail.com>
2020-08-19 20:55:21 +0000

    added letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i] || typeof arrayOne[i] !== typeof arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅ Assertion Pased: ${arrayOne} === ${arrayTwo} ✅`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo} ❌`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      delete results[sentence[i]];
+    } else if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
