309b2298e4adaebebf03eb52bb09cea950779bcd
Christopher <christopherstoy@gmail.com>
2020-08-24 20:27:19 +0000

    updated and refactored

--- middle.js
+++ middle.js
@@ -1,41 +1,17 @@
-//Test Assertion Functions
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i] || typeof arrayOne[i] !== typeof arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅ Assertion Pased: ${arrayOne} === ${arrayTwo} ✅`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arrayOne} !== ${arrayTwo} ❌`);
-  }
-};
-
-//Actual Function
-const middle = function(array) {
-  let middle = [];
+const middle = (array) => {
+  let middleElements = [];
   if (array.length <= 2) {
-    return middle;
+    return middleElements;
   } else if (array.length % 2 !== 0) {
-    middle.push(array[Math.floor(array.length / 2)]);
+    middleElements.push(array[Math.floor(array.length / 2)]);
   } else if (array.length % 2 === 0) {
-    middle.push(array[Math.floor((array.length - 1) / 2)]);
-    middle.push(array[Math.floor(array.length / 2)]);
+    middleElements.push(array[Math.floor((array.length - 1) / 2)]);
+    middleElements.push(array[Math.floor(array.length / 2)]);
   }
-  return middle;
+  return middleElements;
 };
 
-//Test Code
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), "3");
+module.exports = middle;
