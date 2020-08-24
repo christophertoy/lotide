9db0e284dc0f8a5bfe3a13cfe901beb49e0404e8
Christopher <christopherstoy@gmail.com>
2020-08-24 21:45:30 +0000

    added functionality to work with mocha/chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), 3);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), "3");
+describe ('#middle', () => {
+  it ('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it ('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+  it ('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+  it ('returns 3 does not equal [3] for [1, 2, 3, 4, 5]', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5]), 3)
+  });
+});
