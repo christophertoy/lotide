cc639919241663add553ed06847516340a49c118
Christopher <christopherstoy@gmail.com>
2020-08-24 15:58:15 +0000

    new test files for modularizing code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual')
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("lol", "lol");
+assertEqual("Chris", "Raine");
+assertEqual(2,4);
+assertEqual(9999,9999);
