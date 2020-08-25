53a094418ed174009d03ced9cf23f986bac837fa
Christopher <christopherstoy@gmail.com>
2020-08-25 03:20:28 +0000

    refactored and added mocha/chai asserts for function

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const letterPositions = require('../letterPositions');
+
+describe("letterPositions", () => {
+  it ('returns {h: [0], e: [1], l: [2, 3], o: [4]} for "hello"', () => {
+   assert.deepEqual(letterPositions("hello"), {h: [0], e: [1], l: [2, 3], o: [4]});
+  });
+  it ('returns [4] for the letter "o"', () => {
+   assert.deepEqual(letterPositions("hello").o, [4]);
+  });
+});
+
